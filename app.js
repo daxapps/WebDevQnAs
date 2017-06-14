17ad10b97eca798ea7016800665708b751be3303
Jason Crawford <daxapps777@gmail.com>
2017-06-14 10:29:04 -0500

    Create User model, Configure passport

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const User = require('./models/user');
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+
+
+mongoose.connect('mongodb://dax:password@ds123722.mlab.com:23722/webdevqna');
 const app = express();
-
-
 app.set('view engine', 'ejs');
+
+app.use(require("express-session")({
+    secret: "asdgasgsafhsdhh",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', function(req, res) {
 	res.render("home");
