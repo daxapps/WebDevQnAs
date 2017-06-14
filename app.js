23f71a48379281ce644c362d7344f8e7611f5fb7
Jason Crawford <daxapps777@gmail.com>
2017-06-14 11:01:10 -0500

    Add register routes and form

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const passportLocalMongoose = require("passport-local-mongoose");
 mongoose.connect('mongodb://dax:password@ds123722.mlab.com:23722/webdevqna');
 const app = express();
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
     secret: "asdgasgsafhsdhh",
     resave: false,
@@ -25,6 +25,10 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//============
+// ROUTES
+//============
+
 app.get('/', function(req, res) {
 	res.render("home");
 });
@@ -33,8 +37,38 @@ app.get('/secret', function(req, res) {
 	res.render('secret');
 })
 
+// Auth Routes
+
+//show sign up form
+app.get("/register", function(req, res){
+   res.render("register"); 
+});
+
+//handling user sign up
+app.post("/register", function(req, res){
+	req.body.username
+	req.body.password
+  User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render('register');
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/secret");
+    });
+  });
+});
+
 
 
 app.listen(process.env.PORT || 8080, function() {
 	console.log('server started.......');
 });
+
+
+
+
+
+
+
+
