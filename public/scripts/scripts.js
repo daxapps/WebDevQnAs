1cdd84f80b9c8bcd5465393212be4b6c454e6291
Jason Crawford <daxapps777@gmail.com>
2017-06-25 20:37:58 -0500

    Present Login form via modal

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,3 +1,15 @@
+//Login Modal Handler
+$('#login').on('click', function(event) {
+	event.preventDefault();
+	$('.login-modal').fadeIn('slow');
+});
+
+//keep focus on login modal
+$('#login button').submit(function(event) {
+	event.preventDefault();
+	$('#user').focus();
+	$('.login-modal').fadeOut('slow');
+});
 
 $("#show-answers-btn").on("click", function(event) {
 	event.preventDefault();	
